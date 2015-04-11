72cfaa028bf35d651552a2aa65288567d4382c07
Adrien AGGERY <adrien@aggery.com>
2015-04-12 00:28:15 +0200

    auth fix

--- api/dynamic-content/auth.php
+++ api/dynamic-content/auth.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if (!empty($_SESSION("user_id")) && !empty($_SESSION("username")))
+if (!empty($_SESSION["user_id"]) && !empty($_SESSION["username"]))
 	echo '<p>Logged in as ' . strtoupper($_SESSION["username"]);
 else
 	echo '<a class="auth-button login-button">LOGIN</a><p>OR</p><a class="auth-button register-button">REGISTER</a>';
