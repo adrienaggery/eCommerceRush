3cab3d1541a92932668df081220000da44878856
Adrien AGGERY <adrien@aggery.com>
2015-04-11 21:22:22 +0200

    new base for frontend

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,54 +9,79 @@ session_start();
     <link rel="stylesheet" href="public/styles/main.css">
 </head>
 <body>
-    <header>
-        <div class="fluid-wrapper">
-            <div class="auth">
-                <a class="auth-button login-button" href="#">LOGIN</a>
-                <p>OR</p>
-                <a class="auth-button register-button" href="#">REGISTER</a>
-            </div>
-        </div>
-    </header>
-    <section class="subheader">
-        <div class="fluid-wrapper">
-            <img class="logo" src="http://placehold.it/130x35" />
-            <input class="searchbar" type="text" name="search" placeholder="Search Product...">
-            <img class="search-logo" src="http://placehold.it/40x40" />
-        </div>
-    </section>
-    <section class="menus-bar">
-        <div class="fluid-wrapper">
-            <div class="home-button">
-                <a class="home" href="#">HOME</a>
-            </div><!-- WP
-            --><div class="home-button">
-                <a class="aboutus" href="#">ABOUT US</a>
-            </div><!-- WP
-            --><div class="home-button">
-                <a class="contactus" href="#">CONTACT US</a>
-            </div><!--
-            --><div class="shop-panel">
-                <img class="shopcart-logo" src="http://placehold.it/40x40" />
-            </div>
-        </div>
-    </section>
-    <br />
-    <section class="bottom">
-        <div class="fluid-wrapper">
-            <div class="men-collect">
-                <a href="#">
-                <img src="public/images/mens-collect.jpg" >
-                <p>MEN'S COLLECTION</p>
-                </a>
-            </div>
-            <div class="women-accesories">
-                <a href="#">
-                <img src="public/images/womens-accesories.jpg" >
-                <p>WOMEN'S ACCESORIES</p>
-                </a>
-            </div>
-        </div>
-    </section>
+	<header class="header">
+		<div class="fluid-wrapper">
+			<div class="auth">
+				<a class="auth-button login-button">LOGIN</a>
+				<p>OR</p>
+				<a class="auth-button register-button">REGISTER</a>
+			</div>
+		</div>
+	</header>
+	<header class="subheader">
+		<div class="fluid-wrapper">
+			<img class="site-logo" height="35" src="http://placehold.it/130x35" />
+			<div class="search">
+				<form>
+					<input class="searchbar" placeholder="Search product">
+					<input class="submit-button" type="submit">
+				</form>
+			</div>
+		</div>
+	</header>
+	<header class="subsubheader">
+		<div class="fluid-wrapper">
+			<nav>
+				<ul>
+					<li><a href="#">Accueil</a></li>
+					<li><a href="#">Homme</a></li>
+					<li><a href="#">Femme</a></li>
+					<li><a href="#">Catégories</a>
+						<ul>
+							<li><a href="#">Vestes</a></li>
+							<li><a href="#">Pulls</a></li>
+							<li><a href="#">Chemises</a></li>
+							<li><a href="#">...</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Contactez nous</a></li>
+				</ul>
+			</nav>
+			<div class="cart">
+				<img class="cart-icon" height="32" width="32" src="http://placehold.it/32x32" />
+				<p>x items<br/>$10.00</p>
+			</div>
+		</div>
+	</header>
+	<section class="shops">
+		<div class="fluid-wrapper">
+			<a class="shop-big shop-homme">SHOP HOMME</a>
+			<a class="shop-big shop-femme">SHOP FEMME</a>
+		</div>
+	</section>
+	<section class="featured-title">
+		<div class="fluid-wrapper">
+			<p>Featured</p>
+			<div class="line"></div>
+		</div>
+	</section>
+	<section class="featured-products">
+		<div class="fluid-container">
+			<div class="product-card">
+				<div class="image"></div>
+				<div class="description">
+					<p class="name">Chemise laine</p>
+					<p class="price">$25.00</p>
+				</div>
+				<a class="addtocart"></a>
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div class="fluid-container">
+			<p>Copyright 2015 &copy Jason & Adrien</p>
+			<a class="backoffice-link">Backoffice</a>
+		</div>
+	</footer>
 </body>
 </html>
