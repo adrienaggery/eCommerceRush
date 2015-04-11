7ba9974d825cccc9bc03e6281b4dc27335798d70
Jason BUSSON <jabusson@e1r9p14.42.fr>
2015-04-11 13:21:13 +0200

    jason-Updatefront

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
 	<title>ft_minishop</title>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:500,300,400' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="public/styles/reset.css">
+    <link rel="stylesheet" href="public/styles/main.css">
 </head>
 <body>
-BONJOUR
+    <header>
+        <div class="fluid-wrapper">
+            <div class="auth">
+                <a class="auth-button login-button" href="#">LOGIN</a>
+                <p>OR</p>
+                <a class="auth-button register-button" href="#">REGISTER</a>
+            </div>
+        </div>
+    </header>
 </body>
 </html>
