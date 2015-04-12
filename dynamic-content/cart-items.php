383e17fa7dfa8ebc3afc74d06b2e11ccf8a06c05
Adrien AGGERY <adrien@aggery.com>
2015-04-12 20:26:03 +0200

    items count in cart

--- dynamic-content/cart-items.php
+++ dynamic-content/cart-items.php
@@ -11,7 +11,7 @@ foreach($_SESSION['cart'] as $key => $value)
     mysqli_stmt_bind_result($query, $product_id, $product_name, $product_price, $product_image);
     mysqli_stmt_execute($query);
     while (mysqli_stmt_fetch($query)){
-        echo '<div class="product-card"><div class="image" style="background: url(\'' . $product_image . '\') center no-repeat; background-size: cover;"></div><div class="description"><p class="categories">Chemise, Homme</p><p class="name">' . $product_name . '</p><p class="price">$' . $product_price . '</p><div class="cart-act-wrapper"><a class="cart-act" href="handlers/addtocart.php?product_id=' . $product_id .'">+ 1</a>|<a class="cart-act" href="handlers/subfromcart.php?product_id=' . $product_id .'">- 1</a>|<a class="cart-act" href="handlers/delfromcart.php?product_id=' . $product_id .'">Remove</a></div></div></div>';
+        echo '<div class="product-card"><div class="image" style="background: url(\'' . $product_image . '\') center no-repeat; background-size: cover;"></div><div class="description"><p class="categories">Chemise, Homme</p><p class="name">' . $product_name . '</p><p class="price">$' . $product_price . '</p><div class="cart-act-wrapper"><a class="cart-act" href="handlers/addtocart.php?product_id=' . $product_id .'">+ 1</a>|<a class="cart-act" href="handlers/subfromcart.php?product_id=' . $product_id .'">- 1</a>|<a class="cart-act" href="handlers/delfromcart.php?product_id=' . $product_id .'">Remove</a>|<a class="cart-act" href>' . $value . ' item(s)</a></div></div></div>';
     }
 }
 
