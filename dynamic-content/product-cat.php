9ebdeb8aabd4fa5260e2f9f3118c9528c3c95ad3
Jason BUSSON <jabusson@e1r9p15.42.fr>
2015-04-12 20:17:28 +0200

    bonjour

--- dynamic-content/product-cat.php
+++ dynamic-content/product-cat.php
@@ -1,16 +1,32 @@
 <?php
-$_GET['cat_id'] = "5";
+$_GET['cat_id'] = "9";
 include("db/connect.php");
 
 $query = mysqli_stmt_init($db);
 mysqli_stmt_prepare($query, 'SELECT id, product_name, product_price, product_image, product_categories FROM products');
 mysqli_stmt_bind_result($query, $product_id, $product_name, $product_price, $product_image, $product_categories);
 mysqli_stmt_execute($query);
-$product_categories = explode(",", $product_categories);
 while (mysqli_stmt_fetch($query)){
+    $product_categories = explode(",", $product_categories);
     if (in_array($_GET['cat_id'], $product_categories))
     {
-        echo '<div class="product-card"><div class="image" style="background: url(\'' . $product_image . '\') center no-repeat; background-size: cover;"></div><div class="description"><p class="categories">Chemise, Homme</p><p class="name">' . $product_name . '</p><p class="price">$' . $product_price . '</p><a class="addtocart" href="handlers/addtocart.php?product_id=' . $product_id .'">Ajouter au panier</a></div></div>';
+        if (in_array('1', $product_categories))
+            $sexe = " Homme";
+        else if (in_array('2', $product_categories))
+            $sexe = " Femme";
+        if (in_array('3', $product_categories))
+            $prod = "Chemise,";
+        if (in_array('4', $product_categories))
+            $prod = "Veste,";
+        if (in_array('5', $product_categories))
+            $prod = "Pull,";
+        if (in_array('6', $product_categories))
+            $prod = "T-Shirt,";
+        if (in_array('7', $product_categories))
+            $prod = "Pantalon,";
+        if (in_array('9', $product_categories))
+            $prod = "Jean,";
+        echo '<div class="product-card"><div class="image" style="background: url(\'' . $product_image . '\') center no-repeat; background-size: cover;">   </div><div class="description"><p class="categories">' . $prod . $sexe . '</p><p class="name">' . $product_name . '</p><p class="price">$' .   $product_price . '</p><a class="addtocart" href="handlers/addtocart.php?product_id=' . $product_id .'">Ajouter au panier</a></div></div>';
     }
 }
 mysqli_stmt_close($query);
