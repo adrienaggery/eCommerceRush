c9d512886cefcc39762e3913785fd374d06c8fa7
Adrien AGGERY <adrien@aggery.com>
2015-04-12 03:36:36 +0200

    dyn content update

--- api/dynamic-content/featured.php
+++ api/dynamic-content/featured.php
@@ -3,7 +3,7 @@
 include("../dbconnect.php");
 
 $query = mysqli_stmt_init($db);
-mysqli_stmt_prepare($query, 'SELECT id, product_name, product_price, product_image FROM products WHERE featured=1 LIMIT 0,4 ORDER BY');
+mysqli_stmt_prepare($query, 'SELECT id, product_name, product_price, product_image FROM products WHERE featured=1 LIMIT 0,4 ORDER BY last_modif');
 mysqli_stmt_bind_result($query, $product_id, $product_name, $product_price, $product_image);
 mysqli_stmt_execute($query);
 while (mysqli_stmt_fetch($query)){
