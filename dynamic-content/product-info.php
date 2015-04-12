bae7d50046d60ff8d8c07c946da1dc2d36ac70c7
Jason BUSSON <jabusson@e1r9p14.42.fr>
2015-04-12 04:18:37 +0200

    jason product.php

--- dynamic-content/product-info.php
+++ dynamic-content/product-info.php
@@ -1,10 +1,15 @@
 <?php
+$_GET['product_id'] = "1";
 	if (!empty($_GET["product_id"])){
-		include("../dbconnect.php");
-		
+		include("../api/dbconnect.php");
+
 		$query = mysqli_stmt_init($db);
 		mysqli_stmt_prepare($query, 'SELECT * FROM products WHERE id=?');
 		mysqli_stmt_bind_param($query, 'i', $_GET["product_id"]);
-		mysqli_stmt_bind_result($query, $product_info);
+		mysqli_stmt_bind_result($query, $product_info['id'], $product_info['name'], $product_info['desc'], $product_info['price'], $product_info['categories'], $product_info['image'], $product_info['stock'], $product_info['featured'], $product_info['modif']);
+        mysqli_stmt_execute($query);
+        mysqli_stmt_fetch($query);
+        mysqli_stmt_close($query);
+        mysqli_close($db);
 	}
 ?>
